342551cdc747be27a6a486cc6e1720a4c4522feb
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2025-05-18 17:55:21 +0530

    first commit

---
+++ Contribute.md
@@ -0,0 +1,14 @@
+## manual installation
+    -- Instal nodejs locally
+    -- clone the rep
+    - install the depnedecises
+    - start the db locally
+    using docker
+    npx prism migrate
+
+
+
+
+
+## Docker installation
+    
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import { PrismaClient } from './generated/prisma';
+
+const app=express();
+const PORT=3000;
+
+const prismaClient= new PrismaClient();
+
+app.get('/',async (req, res) => {
+   const data=await prismaClient.user.findMany();
+   console.log(data);
+    res.json(data);
+}
+);  
+   
+
+app.post('/api', async(req, res) => {
+     await prismaClient.user.create({
+        data:{
+        username:Math.random().toString(36).substring(2, 15) ,
+        password: Math.random().toString(36).substring(2, 15),
+    }
+        
+})
+});
+
+app.listen(PORT);
